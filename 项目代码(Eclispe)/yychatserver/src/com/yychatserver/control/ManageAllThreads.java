08d7af35d3ecd3054a8bb42ee1133c16973ecea0
黄廉升 <93899840+Okami888@users.noreply.github.com>
2022-06-04 17:37:06 +0800

    Add files via upload

---
+++ 项目代码(Eclispe)/yychatserver/src/com/yychatserver/control/ManageAllThreads.java
@@ -0,0 +1,54 @@
+package com.yychatserver.control;
+
+
+import java.util.Hashtable;
+import java.util.Iterator;
+
+
+import com.yychatserver.control.ServerReceiverThread;
+
+	/**
+	 * 管理所有与客户端的线程
+	 */
+	public class ManageAllThreads {
+		
+		
+
+	    public static Hashtable<String,ServerReceiverThread> hmSocket = new Hashtable<>();
+	    
+	 
+		public static Hashtable<String, ServerReceiverThread> getServerReceiverThreads() {
+	        return hmSocket;
+	    }
+		public static ServerReceiverThread getServerReceiverThread(String uid){
+	        return hmSocket.get(uid);
+	    }
+		
+		
+	    public static void addServerReceiverThread(String uid, ServerReceiverThread thread){
+	    	hmSocket.put(uid,thread);
+	    }    
+	    public static void removeServerReceiverThread(String uid){
+	    	hmSocket.remove(uid);
+	    }
+
+	    /**
+	     * 返回当前在线全部用户
+	     * @return
+	     */
+	    public static String getOnLineList(){
+	        StringBuilder sb = new StringBuilder();
+	        String itString="";
+	        Iterator<String> it = hmSocket.keySet().iterator();//拿到键的信息，即所有上线好友名称的一张列表//创建迭代器对象
+	        while(it.hasNext()){//注意迭代器的循环中只能出现一次it.next否则会报错
+	        	
+	        	itString =it.next().toString();
+	        	if(hmSocket.get(itString).isHidden()) {//跳过隐身的好友检索
+	        		continue;
+	        	}
+	        	sb.append(itString+" ");//onLineFriend为一串空格间隔的在线好友字符串                  
+	        }
+	        return sb.toString();
+	    }
+	}
+
